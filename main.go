091155e73c0b1e7e637d504c0026e00a8b01a760
VVatchful <tdbt1122@gmail.com>
2024-09-23 19:21:43 -0400

    Made a repl

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-func sayHi() {
-  fmt.Println("Hello World!")
+func main() {
+	fmt.Println("You found someones pokedex!")
+
+	commands := map[string]func(){
+		"help": printHelp,
+		"exit": exitREPL,
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("pokedex > ")
+
+		scanner.Scan()
+		input := strings.TrimSpace(scanner.Text())
+
+		if cmd, found := commands[input]; found {
+			cmd()
+		} else {
+			fmt.Println("Unknown command. Type 'help' to see available commands.")
+		}
+	}
 }
+
+func printHelp() {
+	fmt.Println("Available commands:")
+	fmt.Println("  help  - Prints this help message.")
+	fmt.Println("  exit  - Exits the REPL.")
+}
+
+func exitREPL() {
+	fmt.Println("Goodbye!")
+	os.Exit(0) }
+
